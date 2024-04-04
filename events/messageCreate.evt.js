c166c2f8580c32b3dcc6feed369253fc9a274f1d
Brncray <57870234+Brncray@users.noreply.github.com>
2024-04-04 16:47:50 -0400

    Update messageCreate.evt.js

--- events/messageCreate.evt.js
+++ events/messageCreate.evt.js
@@ -11,16 +11,6 @@ export async function execute(message, client) {
 
     if (message.author.bot) return;
 
-    if (!message.author.member.permissions.has("Administrator")){
-    const highCommandRole = message.guild.roles.cache.get('1225483987024351262');
-    if (!highCommandRole) return;
-    const highCommandMembers = message.guild.members.cache.filter(m => m.roles.cache.has(highCommandRole.id));
-    const mentionedMembers = message.mentions.members;
-    for (const member of mentionedMembers) {
-        if (highCommandMembers.has(member[0])) {
-            await message.reply({ content: "Please do not mention a High Command member."});
-        }
-    }
+    
 }
 
-}
